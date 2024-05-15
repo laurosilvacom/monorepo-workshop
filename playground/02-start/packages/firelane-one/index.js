6dfe8b74592a2e40358a83930e0aeafbe76e68ae
Lauro Silva <hey@laurosilva.com>
2024-05-15 15:45:52 -0700

    adding 02 code

---
+++ playground/02-start/packages/firelane-one/index.js
@@ -0,0 +1,7 @@
+function calculateLeaves(branchCount) {
+  return branchCount * 70
+}
+
+let branchCount = 100 // number of branches
+
+console.log(`The tree has approximately: `, calculateLeaves(branchCount), ' leaves 🍃')
