535ae3ab2b6f4641a54e03df267103fda318dc2f
Lauro Silva <hey@laurosilva.com>
2024-09-14 16:03:34 -0700

    update folders

---
+++ playground/01-start/packages/index.js
@@ -0,0 +1,7 @@
+function calculateLeaves(branchCount) {
+  return branchCount * 70
+}
+
+let branchCount = 70 // number of branches
+
+console.log(`The tree has approximately: `, calculateLeaves(branchCount), ' leaves 🍃')
